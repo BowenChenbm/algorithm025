868d5469e01b868e6cc757d55772fe32ebb3f6de
BowenChenbm <35058250+BowenChenbm@users.noreply.github.com>
2021-04-04 22:01:19 +0800

    Update removeDuplicates.py

--- Week_01/removeDuplicates.py
+++ Week_01/removeDuplicates.py
@@ -1,6 +1,7 @@
 # Delete duplicated items in a sorted Array
 # 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
 # first mind: nums = list(set(nums)) 
+# using two poiner: slow and fast
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         slow = 0
