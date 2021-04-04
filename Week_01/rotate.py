f653a4771fd7cda2c2968cc936c273e320bf40f1
BowenChenbm <35058250+BowenChenbm@users.noreply.github.com>
2021-04-04 23:38:09 +0800

    Update rotate.py

--- Week_01/rotate.py
+++ Week_01/rotate.py
@@ -1,6 +1,7 @@
 # rotate array
 #给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
 # First mind: create a new array to save the result then copy back to the origianal one
+# math: (i+k) % length, to get the new position
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         length = len(nums)
