904ea5f1380ce3566ec4005c021bbcce1ea013e4
BowenChenbm <35058250+BowenChenbm@users.noreply.github.com>
2021-04-04 22:18:13 +0800

    Update plusOne.py

--- Week_01/plusOne.py
+++ Week_01/plusOne.py
@@ -1,5 +1,6 @@
 # plus one
 # 给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。你可以假设除了整数 0 之外，这个整数不会以零开头。
+# 利用字符串和数字类型转换，最后考虑前几位都为0的情况
 class Solutions:
     def plusOne(self, digits: List[int]) -> List[int]:
         num = ''.join(str(x) for x in digits)
